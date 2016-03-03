fe0f49d446ca6a5574a9b6a940cdeb2b4d1a17a6
Anik khan <anik9280@gmail.com>
2016-03-03 20:18:55 +0600

    ido: Copy prebuilts to correct location

--- full_ido.mk
+++ full_ido.mk
@@ -28,6 +28,11 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Kernel
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/kernel:kernel \
+    $(LOCAL_PATH)/recovery/dt.img:dt.img
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ido
 PRODUCT_NAME := full_ido
